9b978b3b4f007bf4050b1b4962ecd979b927521f
Francisco Cordeiro <franciscordeiro99@gmail.com>
2021-05-16 18:21:16 +0100

    added search by text

--- src/main/kotlin/UI/Visualizer.kt
+++ src/main/kotlin/UI/Visualizer.kt
@@ -5,19 +5,30 @@ import JSONValue
 import org.eclipse.swt.SWT
 import org.eclipse.swt.events.SelectionAdapter
 import org.eclipse.swt.events.SelectionEvent
+import org.eclipse.swt.graphics.Color
 import org.eclipse.swt.layout.GridLayout
 import org.eclipse.swt.widgets.*
 import visitors.CreateUITree
 import visitors.PrintJSONVisitor
+import org.eclipse.swt.widgets.TreeItem
+
+
+
 
 
 fun main() {
-    data class Point (val x :Int, val y :Int)
-    data class Person (val name: String, val age: Int, val location: Point, val pepe: List<String>, val hist: List<Point>)
+    data class Point(val x: Int, val y: Int)
+    data class Person(
+        val name: String,
+        val age: Int,
+        val location: Point,
+        val pepe: List<String>,
+        val hist: List<Point>
+    )
 
     val arr = listOf<String>("potato", "poteto")
-    val arrP = listOf<Point>(Point(1, 1), Point(5,5))
-    val test = Person("Francisco", 21, Point(2, 2),arr, arrP )
+    val arrP = listOf<Point>(Point(1, 1), Point(5, 5))
+    val test = Person("Francisco", 21, Point(2, 2), arr, arrP)
 
 
     val o = JSONGenerator().instantiate(test)
@@ -35,30 +46,48 @@ class Structure() {
         shell = Shell(Display.getDefault())
         shell.setSize(1000, 1000)
         shell.text = "File tree skeleton"
-        shell.layout = GridLayout(2,false)
+        shell.layout = GridLayout(2, false)
 
         tree = Tree(shell, SWT.SINGLE or SWT.BORDER)
         val label = Label(shell, SWT.NONE)
 
         tree.addSelectionListener(object : SelectionAdapter() {
             override fun widgetSelected(e: SelectionEvent) {
-                //TODO: instanciate new window with PrintJSON visitor
                 val visitor = PrintJSONVisitor()
                 val item = tree.selection.first()
                 (item.data as JSONValue).accept(visitor)
                 label.text = visitor.output
 
+                shell.setSize(1000, 500) //TODO: change this to set window size
                 shell.layout(true) //update layout
 
             }
         })
 
 
+        val searchInput = Text(shell, SWT.BORDER)
+
+        searchInput.addModifyListener { event -> // Get the widget whose text was modified
+            val text = event.widget as Text
+
+            getAllItems(tree)
+                .forEach {
+                    if (it.text.contains(text.text) && text.text.isNotBlank())
+                        it.setBackground(Color(255, 255, 102))
+                    else
+                        it.setBackground(null)
+
+                }
+
+
+        }
+
+
     }
 
     // auxiliar para profundidade do nó
     fun TreeItem.depth(): Int =
-        if(parentItem == null) 0
+        if (parentItem == null) 0
         else 1 + parentItem.depth()
 
 
@@ -76,9 +105,9 @@ class Structure() {
     }
 }
 
-fun Tree.expandAll() = traverse { it.expanded = true }
+private fun Tree.expandAll() = traverse { it.expanded = true }
 
-fun Tree.traverse(visitor: (TreeItem) -> Unit) {
+private fun Tree.traverse(visitor: (TreeItem) -> Unit) {
     fun TreeItem.traverse() {
         visitor(this)
         items.forEach {
@@ -88,4 +117,21 @@ fun Tree.traverse(visitor: (TreeItem) -> Unit) {
     items.forEach { it.traverse() }
 }
 
+private fun getAllItems(tree: Tree) : MutableList<TreeItem>{
+    val allItems = mutableListOf<TreeItem>()
 
+    for (item in tree.items)
+        getAllItems(item, allItems)
+
+    return allItems
+}
+
+private fun getAllItems(currentItem: TreeItem, allItems: MutableList<TreeItem>) {
+
+    val children = currentItem.items
+
+    for (child in children) {
+        allItems.add(child)
+        getAllItems(child, allItems)
+    }
+}
